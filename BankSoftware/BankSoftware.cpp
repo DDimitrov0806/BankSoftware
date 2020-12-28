f6e0287169b4945ae4a8ccb54a315dc173b1d5d3
DDimitrov0806 <d.dimitrov0806@abv.bg>
2020-12-28 17:11:04 +0200

    Add Main Menu

--- BankSoftware/BankSoftware.cpp
+++ BankSoftware/BankSoftware.cpp
@@ -1,7 +1,64 @@
 
 #include <iostream>
 
+using namespace std;
+
+void login()
+{
+
+}
+
+void regist()
+{
+
+}
+
+void quit()
+{
+
+}
+
+void mainMenu()
+{
+    char action;
+
+    cout << "\tPlease select one of the following actions: " << endl;
+    cout << "==========================================================" << endl;
+
+    cout << "L-login" << endl;
+    cout << "R-register" << endl;
+    cout << "Q-quit" << endl;
+    cout << "==========================================================" << endl;
+
+    cin >> action;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if (action == 'L' || action == 'l')
+    {
+        cout << "login";
+        login();
+    }
+
+    if (action == 'R' || action == 'r')
+    {
+        cout << "register";
+        regist();
+    }
+
+    if (action == 'Q' || action == 'q')
+    {
+        cout << "quit";
+        quit();
+    }
+
+    cout << "Please select valid action!"<<endl;
+
+    mainMenu();
+
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    mainMenu();
+
 }
