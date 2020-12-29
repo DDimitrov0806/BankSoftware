f234eed026fd642360d211705693871607ecff1b
DDimitrov0806 <d.dimitrov0806@abv.bg>
2020-12-29 21:10:11 +0200

    Add login

--- BankSoftware/BankSoftware.cpp
+++ BankSoftware/BankSoftware.cpp
@@ -1,14 +1,48 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-void login()
+void actionMenu(fstream& file)
 {
-
+    cout << "ActionMenu";
 }
 
-void regist()
+void login(fstream& file)
+{
+    string fileLine, username, password,compareString;
+
+    cout << "Username:" << endl;
+    cin >> username;
+    
+    cout << "Password:" << endl;
+    cin >> password;
+
+    compareString.append(username);
+    compareString.append(":");
+    compareString.append(password);
+
+    while (getline(file,fileLine))
+    {
+
+        if (fileLine.compare(0, compareString.size(), compareString) == 0)
+        {
+            actionMenu(file);
+            return;
+        }
+    }
+
+    cout << "Your username or/and password is incorrect!"<<endl;
+
+    file.clear();
+    file.seekg(0, file.beg);
+
+    login(file);
+}
+
+void regist(fstream& file)
 {
 
 }
@@ -21,6 +55,16 @@ void quit()
 void mainMenu()
 {
     char action;
+
+    string fileName = "users.txt";
+    fstream file;
+    file.open(fileName, ios::out | ios::in);
+
+    if (!file.is_open())
+    {
+        cout << "Could not find " + fileName + " !\nPlease make sure the file is in the correct directory and is named correctly!"<<endl;
+        return;
+    }
 
     cout << "\tPlease select one of the following actions: " << endl;
     cout << "==========================================================" << endl;
@@ -35,27 +79,32 @@ void mainMenu()
 
     if (action == 'L' || action == 'l')
     {
-        cout << "login";
-        login();
+        
+        login(file);
+        return;
     }
 
     if (action == 'R' || action == 'r')
     {
         cout << "register";
-        regist();
+        regist(file);
+        return;
     }
 
     if (action == 'Q' || action == 'q')
     {
         cout << "quit";
         quit();
+        return;
     }
 
-    cout << "Please select valid action!"<<endl;
+    cout << "Please select valid action!"<<endl<<endl;
 
     mainMenu();
 
 }
+
+
 
 int main()
 {
